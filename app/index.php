71b8b7c024d0b68d993462ea57161de5f133b9c7
WaalerThomas <superxgenesis@gmail.com>
2024-01-15 20:39:21 +0100

    Made it ready for use
    Using docker to make it easier for everyone to have a local development copy running

---
+++ app/index.php
@@ -0,0 +1,29 @@
+<?php
+
+require 'config.php';
+
+$host = "mysql";
+$charset = "utf8";
+$port = "3306";
+
+try {
+    $pdo = new PDO(
+        dsn: "mysql:host=$host;dbname=$DB_NAME;charset=$charset;port=$port",
+        username: "someuser",
+        password: "$DB_PASSWORD"
+    );
+
+    $persons = $pdo->query("SELECT * FROM Persons");
+
+    echo '<pre>';
+    foreach ($persons->fetchAll(PDO::FETCH_ASSOC) as $person) {
+        print_r($person);
+    }
+    echo '</pre>';
+
+} catch (PDOException $e) {
+    throw new PDOException(
+        message: $e->getMessage(),
+        code: (int)$e->getCode()
+    );
+}
