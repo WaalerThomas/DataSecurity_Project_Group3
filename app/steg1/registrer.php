d6c55ded6c2a0c9306bcbc238ad7d4d73100dc51
Paxic23 <100602652+Paxic23@users.noreply.github.com>
2024-01-23 19:17:00 +0100

    Fixed registrer.php
    Made the page toggleable between foreleser and student layout (registrering)

--- app/steg1/registrer.php
+++ app/steg1/registrer.php
@@ -31,19 +31,54 @@ session_start();
     <?php
         unset($_SESSION["errorMessage"]);
     }
+
+    $a = '1';
+    $b = '2';
+
+    echo '<a href="registrer.php?type=' . $a . '">Student</a>';
+    echo ' ';
+    echo '<a href="registrer.php?type=' . $b . '">Foreleser</a>';
+
+
+    if(isset($_GET['type'])) {
+        if($_GET['type'] == '1') {
+            echo '<form action="registrer-action.php" method="post">';
+            echo '<label>Fullt navn:</label>';
+            echo '<input type="text" id="full_name" name="full_name" required><br><br>';
+            echo '<label>E-post:</label>';
+            echo '<input type="email" id="email" name="email" required><br><br>';
+            echo '<label>Passord:</label>';
+            echo '<input type="password" id="password" name="password" required><br><br>';
+            echo '<label>Skriv Passord igjen:</label>';
+            echo '<input type="password" id="rep_password" name="rep_password" required><br><br>';
+            echo '<input type="submit" value="Submit" name="registrer_student">';
+            echo '</form>';
+        }
+        else {
+            echo '<form action="registrer-action.php" method="post">';
+            echo '<label>Fullt navn:</label>';
+            echo '<input type="text" id="full_name" name="full_name" required><br><br>';
+            echo '<label>E-post:</label>';
+            echo '<input type="email" id="email" name="email" required><br><br>';
+            echo '<label>Passord:</label>';
+            echo '<input type="password" id="password" name="password" required><br><br>';
+            echo '<label>Skriv Passord igjen:</label>';
+            echo '<input type="password" id="rep_password" name="rep_password" required><br><br>';
+            echo '<label>Emnekode:</label>';
+            echo '<input type="text" id="emnekode" name="emnekode" required><br><br>';
+            echo '<label>Emnepin:</label>';
+            echo '<input type="text" id="emnepin" name="emnepin" required><br><br>';
+            echo '<label>Profilbilde: </label>';
+            echo '<input type="file" name="fileToUpload" id="fileToUpload">';
+            echo '<br><br>';
+            echo '<input type="submit" value="Submit" name="registrer_foreleser">';
+            echo '</form>';
+        }
+
+    }
     ?>
 
-    <form action="registrer-action.php" method="post">
-        <label>Fullt navn:</label>
-        <input type="text" id="full_name" name="full_name" required><br><br>
-        <label>E-post:</label>
-        <input type="email" id="email" name="email" required><br><br>
-        <label>Passord:</label>
-        <input type="password" id="password" name="password" required><br><br>
-        <label>Skriv Passord igjen:</label>
-        <input type="password" id="rep_password" name="rep_password" required><br><br>
-        <input type="submit" value="Submit" name="register_user">
-    </form>
+    
 </div>
 
 </body>
