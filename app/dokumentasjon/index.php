055c73cce5a39f54ced78cad8674537f6ec182c6
TomWaa <superxgenesis@gmail.com>
2024-02-07 11:49:46 +0100

    Started on documentation

--- app/dokumentasjon/index.php
+++ app/dokumentasjon/index.php
@@ -9,7 +9,25 @@
 
 <h1>Dokumentasjon</h1>
 <h2>Medlemmer</h2>
-Ah
+<p>Jonas Emil Høgseth - </p>
+<p>Thomas Kinn - </p>
+<p>Tobias William Sommervold - </p>
+<p>Thomas Waaler - thomaswa@hiof.no</p>
+<p>Petter Viktor Åmot - </p>
+
+<h2>Hva har blitt gjort</h2>
+<p>
+    Under utviklingen har vi benyttet oss av følgende verktøy og tjenester:
+    <ul>
+        <li><b>GitHub</b> - For versjonskontrollering og samarbeid</li>
+        <li><b>Docker</b> - For at vi alle skal ha likt arbeidsmiljø som kjører på linux, sånn som den virtuelle serveren.</li>
+        <li><b>MySQL Workbench</b> - Design av database</li>
+    </ul>
+</p>
+<p>
+    <!-- Skriv om hva som er laget og hva vi har gjort, men ikke beskriv sikkerhetsløsningen, kjente hull osv -->
+    
+</p>
 
 </body>
 </html>
